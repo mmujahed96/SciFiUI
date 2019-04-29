0643a77cafb4bc6b0b39625123e244afccda2349
mmujahed96 <C17427304@mydit.ie>
2019-04-29 15:48:14 +0100

    trying P3d

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -81,7 +81,7 @@ public class UI extends PApplet
     {
         //size(800, 800);
         // Use fullscreen instead of size to make your interface fullscreen
-        fullScreen(P3D); 
+        fullScreen(); 
     }
 
     public void setup()
